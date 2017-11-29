d5dd8d167bbf8d1d442789f640c02fd6bf2b4ef6
Ubuntu <danieljameskay@gmail.com>
2017-11-29 15:22:25 +0000

    removed build errors

--- src/article.js
+++ src/article.js
@@ -5,7 +5,7 @@ import './article.css';
 const Article = (article) => {
   return(
       <div id="article" className="article">
-        <img className="img-responsive" src={article.article.image} />
+        <img className="img-responsive" src={article.article.image} alt="{article.artcile.title}"/>
         <h4 id="date" className="date-text">{articleHelper.trimDate(article.article.date)}</h4>
         <h2 id="title" className="title-text">{article.article.title}</h2>
         <p id="body" className="body-text">{article.article.body}</p>
